c747bd3fcbfc937960f35ad5183c2def4173dc8a
dfnerio <diego9816@hotmail.com>
2021-05-18 18:10:52 -0500

    graph scattered clusters and add moons and circles examples

--- A01193624_kmeans.py
+++ A01193624_kmeans.py
@@ -12,7 +12,8 @@ class k_means:
         centroids = {}
 
         for i in range(k):
-            centroids[i] = data[i]
+            # initializes with random centroids
+            centroids[i] = data[np.random.randint(len(data))]
 
         classifications = {}
 
@@ -55,35 +56,87 @@ class k_means:
         return centroids, classifications
 
     def predict(self, data, cent, classifs):
-        dists = [np.linalg.norm(data - cent[c]) for c in cent]
-        classif = dists.index(min(dists))
-        return classif
+        classifications = {}
+        for i in range(len(data)):
+            classifications[i] = []
+
+        for i in range(len(data)):
+            distances = [pow(np.linalg.norm(
+                data[i] - centroids[c]), 2) for c in centroids]
+            classifications[i] = distances.index(min(distances))
+
+        return list(classifications.values())
 
 
 # main
 
-data = pd.read_csv('datasets/blobs.csv')
+# blobs
+
+data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/blobs.csv')
 X = data.to_numpy()
 
-k = 4
+k = 3
 alg = k_means()
 centroids, classifications = alg.fit(X, k, 150)
 
-print("CLASSIFICATIONS", classifications)
-print("CENTROIDS", centroids)
+colors = ['r', 'b', 'g']
 
-colors = ['k', 'b', 'g', 'r']
+for cluster in range(k):
+    for cl_i in range(len(classifications)):
+        if classifications[cl_i] == cluster:
+            plt.scatter(X[cl_i][0], X[cl_i][1], color=colors[cluster])
 
-# plt.scatter(centroids, y=classifications)
-# plt.scatter()
-# plt.ylabel('some numbers')
-# plt.show()
-
-new_point = [-8, 2]
-cluster = alg.predict(new_point, centroids, classifications)
-plt.scatter(new_point[0], new_point[1], color = colors[cluster])
+new_points = [[8, -2], [-10, 4]]
+prediction = alg.predict(new_points, centroids, classifications)
+print("PREDICTIONS", prediction)
+for point_i in range(len(new_points)):
+    plt.scatter(new_points[point_i][0], new_points[point_i][1], color=colors[prediction[point_i]])
 
 for i, c in enumerate(centroids):
-    plt.scatter(centroids[c][0] , centroids[c][1] , s = 80, color = colors[i])
+    plt.scatter(centroids[c][0], centroids[c][1], s=80, color="black")
 
 plt.show()
+
+
+# # moons
+
+# data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/moons.csv')
+# X = data.to_numpy()
+
+# k = 2
+# alg = k_means()
+# centroids, classifications = alg.fit(X, k, 150)
+
+# colors = ['r', 'b']
+
+# for cluster in range(k):
+#     for cl_i in range(len(classifications)):
+#         if classifications[cl_i] == cluster:
+#             plt.scatter(X[cl_i][0], X[cl_i][1], color=colors[cluster])
+
+# for i, c in enumerate(centroids):
+#     plt.scatter(centroids[c][0], centroids[c][1], s=80, color="black")
+
+# plt.show()
+
+
+# # circles
+
+# data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/circles.csv')
+# X = data.to_numpy()
+
+# k = 2
+# alg = k_means()
+# centroids, classifications = alg.fit(X, k, 150)
+
+# colors = ['r', 'b']
+
+# for cluster in range(k):
+#     for cl_i in range(len(classifications)):
+#         if classifications[cl_i] == cluster:
+#             plt.scatter(X[cl_i][0], X[cl_i][1], color=colors[cluster])
+
+# for i, c in enumerate(centroids):
+#     plt.scatter(centroids[c][0], centroids[c][1], s=80, color="black")
+
+# plt.show()
