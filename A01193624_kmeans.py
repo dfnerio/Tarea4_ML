482e6f73362381cb63c8c198710d0e9b6146f450
dfnerio <diego9816@hotmail.com>
2021-05-18 19:33:01 -0500

    add cost and elbow functions + example

--- A01193624_kmeans.py
+++ A01193624_kmeans.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class k_means:
     classifications = {}
     centroids = {}
@@ -25,38 +26,45 @@ class k_means:
         for iteration in range(max_iter):
 
             for i in range(k):
-                self.classifications[i] = [] # initialize each classification index with an empty array
+                # initialize each classification index with an empty array
+                self.classifications[i] = []
 
             for i in range(len(data)):
                 distances = [pow(np.linalg.norm(
-                    data[i] - self.centroids[c]), 2) for c in self.centroids] # calculate distance for each example set and centroid
-                self.classifications[i] = distances.index(min(distances)) # add the cluster index of the minimum distance to the classifications object
+                    data[i] - self.centroids[c]), 2) for c in self.centroids]  # calculate distance for each example set and centroid
+                # add the cluster index of the minimum distance to the classifications object
+                self.classifications[i] = distances.index(min(distances))
 
-            current_centroids = self.centroids # create a copy of the current centroid for further checking
+            # create a copy of the current centroid for further checking
+            current_centroids = self.centroids
 
             for centroid_i in range(len(self.centroids)):
 
-                current = [] # array to keep track of sets in current cluster
+                current = []  # array to keep track of sets in current cluster
 
                 for classification_i in range(len(self.classifications)):
                     if self.classifications[classification_i] == centroid_i:
-                        current.append(data[classification_i]) # append corresponding set to current cluster array
+                        # append corresponding set to current cluster array
+                        current.append(data[classification_i])
 
                 current_centroid = current_centroids[centroid_i]
-                new_centroid = np.average(current, axis=0) # get new centroid by averaging the current array
+                # get new centroid by averaging the current array
+                new_centroid = np.average(current, axis=0)
 
-                if (set(current_centroid) == set(new_centroid)): # if new centroid is equal to current centroid (1/3)
+                # if new centroid is equal to current centroid (1/3)
+                if (set(current_centroid) == set(new_centroid)):
                     optimized = True
                     print('OPTIMIZED AT ITERATION: %s' % str(iteration + 1))
-                    break # then the model is optimized and can stop (2/3)
+                    break  # then the model is optimized and can stop (2/3)
 
-                self.centroids[centroid_i] = new_centroid # if not, assign the new centroid and continue (3/3)
+                # if not, assign the new centroid and continue (3/3)
+                self.centroids[centroid_i] = new_centroid
 
             if optimized:
                 break
 
-        self.classifications = list(self.classifications.values()) # transform the classifications dict into a list of its values
-        return self.centroids, self.classifications
+        # transform the classifications dict into a list of its values
+        return self.centroids, list(self.classifications.values())
 
     def predict(self, data):
         classifications = {}
@@ -70,10 +78,27 @@ class k_means:
 
         return list(classifications.values())
 
+    def cost(self, data):  # calculates the cost of the current model
+        accum = 0
+        for i in range(len(data)):
+            accum += pow(np.linalg.norm(data[i] -
+                         self.centroids[self.classifications[i]]), 2)
+        return accum / len(data)
+
+    def elbow(self, data, min_k, max_k):  # prints the cost for the specified k range
+        costs = {}
+        for i in range(max_k - (min_k-1)):
+            self.fit(data, i + min_k, 150)  # fits the model with the current k
+            # sets the cost in i + min_k so that we can use the keys and values as x and y
+            costs[i + min_k] = self.cost(data)
+
+        plt.plot(list(costs.values()), list(costs.keys()))
+        plt.show()
 
 # main
 
 # blobs
+
 
 data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/blobs.csv')
 X = data.to_numpy()
@@ -93,53 +118,62 @@ new_points = [[8, -2], [-10, 4]]
 prediction = alg.predict(new_points)
 print("PREDICTIONS", prediction)
 for point_i in range(len(new_points)):
-    plt.scatter(new_points[point_i][0], new_points[point_i][1], color=colors[prediction[point_i]])
+    plt.scatter(new_points[point_i][0], new_points[point_i]
+                [1], color=colors[prediction[point_i]])
 
 for i, c in enumerate(centroids):
     plt.scatter(centroids[c][0], centroids[c][1], s=80, color="black")
 
 plt.show()
+
+# elbow method with blobs
+
+# data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/blobs.csv')
+# X = data.to_numpy()
+
+# alg = k_means()
+# alg.elbow(X, 2, 7)
 
 
 # moons
 
-data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/moons.csv')
-X = data.to_numpy()
+# data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/moons.csv')
+# X = data.to_numpy()
 
-k = 2
-alg = k_means()
-centroids, classifications = alg.fit(X, k, 150)
+# k = 2
+# alg = k_means()
+# centroids, classifications = alg.fit(X, k, 150)
 
-colors = ['r', 'b']
+# colors = ['r', 'b']
 
-for cluster in range(k):
-    for cl_i in range(len(classifications)):
-        if classifications[cl_i] == cluster:
-            plt.scatter(X[cl_i][0], X[cl_i][1], color=colors[cluster])
+# for cluster in range(k):
+#     for cl_i in range(len(classifications)):
+#         if classifications[cl_i] == cluster:
+#             plt.scatter(X[cl_i][0], X[cl_i][1], color=colors[cluster])
 
-for i, c in enumerate(centroids):
-    plt.scatter(centroids[c][0], centroids[c][1], s=80, color="black")
+# for i, c in enumerate(centroids):
+#     plt.scatter(centroids[c][0], centroids[c][1], s=80, color="black")
 
-plt.show()
+# plt.show()
 
 
 # circles
 
-data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/circles.csv')
-X = data.to_numpy()
+# data = pd.read_csv('/Users/diego/Desktop/Tarea4/datasets/circles.csv')
+# X = data.to_numpy()
 
-k = 2
-alg = k_means()
-centroids, classifications = alg.fit(X, k, 150)
+# k = 2
+# alg = k_means()
+# centroids, classifications = alg.fit(X, k, 150)
 
-colors = ['r', 'b']
+# colors = ['r', 'b']
 
-for cluster in range(k):
-    for cl_i in range(len(classifications)):
-        if classifications[cl_i] == cluster:
-            plt.scatter(X[cl_i][0], X[cl_i][1], color=colors[cluster])
+# for cluster in range(k):
+#     for cl_i in range(len(classifications)):
+#         if classifications[cl_i] == cluster:
+#             plt.scatter(X[cl_i][0], X[cl_i][1], color=colors[cluster])
 
-for i, c in enumerate(centroids):
-    plt.scatter(centroids[c][0], centroids[c][1], s=80, color="black")
+# for i, c in enumerate(centroids):
+#     plt.scatter(centroids[c][0], centroids[c][1], s=80, color="black")
 
-plt.show()
+# plt.show()
